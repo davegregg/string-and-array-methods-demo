a9653576d9f9d0a7a6a03e33721cc5af71f0d52f
davegregg@gmail.com <davegregg@gmail.com>
2021-08-26 11:01:21 -0400

    update

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Is this JavaScript running?")
+console.log("Is this JavaScript running?".length)
+
+`A "property" is a named quality of an object.`
+
+`A "method" is a function which has been designed to help us solve a common, general problem with a particular data type (strings or arrays, for example).`
+
+`A "callback" is a function which has been provided as input to another function, so that it can be run LATER.`
